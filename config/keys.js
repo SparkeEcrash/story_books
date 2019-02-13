6631af1ba2f0fa7ea2aea9f8c9eb4c8a3ed8bc21
SparkeEcrash <jpark121989@gmail.com>
2019-02-12 16:23:24 -0800

    setup for heroku deployment

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
-module.exports = {
-  mongoURI: 'mongodb://test:password123@ds331145.mlab.com:31145/storybooks-dev',
-  googleClientID: '275630021546-hhnkmkjamprs86pjilt9fiuf0j0oopp8.apps.googleusercontent.com',
-  googleClientSecret: 'Cvjw4mHD4dwwM5gnp7IQCNL7'
+if(process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
 }
+
