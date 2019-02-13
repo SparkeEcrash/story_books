ee9de65f1a6f37657841d58a480e18a65b71b657
SparkeEcrash <jpark121989@gmail.com>
2019-02-12 16:04:55 -0800

    finished comment feature

--- routes/stories.js
+++ routes/stories.js
@@ -9,6 +9,7 @@ const {ensureAuthenticated, ensureGuest} = require('../helpers/auth');
 router.get('/', (req, res) => {
   Story.find({status:'public'})
     .populate('user')
+    .sort({date: 'desc'})
     .then(stories => {
       res.render('stories/index', {
         stories
@@ -22,11 +23,51 @@ router.get('/show/:id', (req, res) => {
     _id: req.params.id 
   })
   .populate('user')
+  .populate('comments.commentUser')
   .then(story => {
-    res.render('stories/show', {
-      story
-    });
+    if(story.status === 'public') {
+      res.render('stories/show', {
+        story
+      });
+    } else {
+      if(req.user){
+        if(req.user.id == story.user._id) {
+          //double equal signs here is necessary because req.user.id is a string and story.user._id is an object that contains a key value pair
+          res.render('stories/show', {
+            story
+          });
+        } else {
+          res.redirect('/stories');
+        }
+      } else {
+      res.redirect('/stories');
+      }
+    }
   });
+});
+
+
+
+// List stories from a user
+router.get('/user/:userId', (req, res) => {
+  Story.find({user: req.params.userId, status: 'public'})
+    .populate('user')
+    .then(stories => {
+      res.render('stories/index', {
+        stories
+      });
+    });
+});
+
+// Logged in users stories
+router.get('/my', (req, res) => {
+  Story.find({user: req.user.id})
+    .populate('user')
+    .then(stories => {
+      res.render('stories/index', {
+        stories
+      });
+    });
 });
 
 // Add Story Form
@@ -40,9 +81,13 @@ router.get('/edit/:id', ensureAuthenticated, (req, res) => {
     _id: req.params.id 
   })
   .then(story => {
-    res.render('stories/edit', {
-      story
-    });
+    if(story.user != req.user.id) {
+      res.redirect('/stories');
+    } else {
+      res.render('stories/edit', {
+        story
+      });
+    }
   });
 })
 
@@ -108,5 +153,27 @@ router.delete('/:id', (req, res) => {
       res.redirect('/dashboard');
     });
 })
+
+// Add Comment
+router.post('/comment/:id', (req, res) => {
+  Story.findOne({
+    _id: req.params.id
+  })
+  .then(story => {
+    const newComment = {
+      commentBody: req.body.commentBody,
+      commentUser: req.user.id
+    }
+
+    // Add to comments array
+    story.comments.unshift(newComment);
+    //unshift to add at beginning
+
+    story.save()
+      .then(story => {
+        res.redirect(`/stories/show/${story.id}`);
+      });
+  });
+});
 
 module.exports = router;
